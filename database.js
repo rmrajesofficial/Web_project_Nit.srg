3474ab5579784f7f73f9a5d55a048ae28d54bce5
VaibhavRY <vaibhav822002@gmail.com>
2023-06-24 14:16:17 +0530

    contactus

---
+++ database.js
@@ -0,0 +1,15 @@
+let States = [];
+let WestBengal = [
+  "Kolkata",
+  "Darjeeling",
+  "Sundarbans",
+  "Siliguri",
+  "Digha",
+  "Kalimpong",
+  "Kurseong",
+  "Durgapur",
+  "Haldia",
+  "Bankura",
+  "Cooch Behar",
+];
+export default WestBengal;
